fc07c7fbe63129b17775376302d4240bb8a95658
Bidur Sahoo <78256127+Bidursahoo@users.noreply.github.com>
2024-10-21 09:06:06 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Ninja Players",
+  description: "Players where you can find other players",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <div>Footers</div>;
+}
+
+export default Footer;
